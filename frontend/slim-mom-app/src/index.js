f77684fa5d02498f88b93374e90ef657dbcc64a9
Cyrus Ducut <cyrus.ducut@fieldforce4.com>
2024-09-18 17:33:18 +0800

    lint

--- frontend/slim-mom-app/src/index.js
+++ frontend/slim-mom-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { persistor, store } from './redux/store';
+import { store } from './redux/store';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
