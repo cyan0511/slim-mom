d83b29fce865753ea9e607d3c80f3bd1727a4d38
sorakazi <145249696+sorakazi@users.noreply.github.com>
2024-10-08 21:52:57 +0800

    update

--- backend/slim-mom-backend/swagger.js
+++ backend/slim-mom-backend/swagger.js
@@ -1,31 +1,10 @@
-<<<<<<< Updated upstream
-=======
 // swagger.js
->>>>>>> Stashed changes
 import swaggerJsDoc from "swagger-jsdoc";
 
-// Get the port from environment variables or default to 5001
-const PORT = process.env.PORT || 5002;
-const SERVER_URL = process.env.SERVER_URL || `http://localhost:${PORT}`;
+const PORT = process.env.PORT || 8080;
 
-// Swagger configuration options
 const swaggerOptions = {
   swaggerDefinition: {
-<<<<<<< Updated upstream
-    openapi: "3.0.0", // OpenAPI version
-    info: {
-      title: "My API", // Title of the API
-      version: "1.0.0", // API version
-      description: "API documentation for my application", // Short description of the API
-    },
-    servers: [
-      {
-        url: SERVER_URL, // Base URL for the API
-      },
-    ],
-  },
-  apis: ["./routes/api/*.js"], // Path to your API route definitions for Swagger annotations
-=======
     openapi: "3.0.0",
     info: {
       title: "My API",
@@ -39,11 +18,8 @@ const swaggerOptions = {
     ],
   },
   apis: ["./routes/api/*.js"], // Path to the API docs (JSDoc comments)
->>>>>>> Stashed changes
 };
 
-// Initialize Swagger documentation
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 
-// Export the generated Swagger documentation
 export default swaggerDocs;
