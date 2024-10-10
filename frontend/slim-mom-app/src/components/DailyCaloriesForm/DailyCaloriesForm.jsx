34edc3d59c84653d383491ecae42c52f0994634c
Cyrus Ducut <cyrus.ducut@fieldforce4.com>
2024-10-10 18:43:13 +0800

    Update Daily Calories Form

--- frontend/slim-mom-app/src/components/DailyCaloriesForm/DailyCaloriesForm.jsx
+++ frontend/slim-mom-app/src/components/DailyCaloriesForm/DailyCaloriesForm.jsx
@@ -5,7 +5,7 @@ import React, {useState} from "react";
 import Modal from "../Modal/Modal";
 
 export const DailyCaloriesForm = () => {
-    const [selectedValue, setSelectedValue] = useState(null);
+    const [formData, setFormData] = useState({});
     const [isOpen, setIsOpen] = useState(false);
     const openModal = () => setIsOpen(true);
     const closeModal = () => setIsOpen(false);
@@ -15,8 +15,13 @@ export const DailyCaloriesForm = () => {
         openModal();
     }
 
-    const handleChange = (event) => {
-        setSelectedValue(+event.target.value);
+    const handleChange = (value, field) => {
+        // setSelectedValue(+event.target.value);
+
+        setFormData({
+            ...formData,
+            [field]: value,
+        });
     };
     const options = [
         {value: 1, label: '1'},
@@ -25,25 +30,37 @@ export const DailyCaloriesForm = () => {
         {value: 4, label: '4'},
     ];
 
+    const calculateIntake = () => {
+        //
+        const {height, currentWeight: weight, age, desiredWeight} = formData;
+        return 10 * weight + 6.25 * height - 5 * age - 161 - 10 * (weight - desiredWeight);
+    }
 
     return (<>
-            <Modal children={<div>test</div>} isOpen={isOpen} onClose={closeModal}  />
+            <Modal children={<div>{calculateIntake()}</div>} isOpen={isOpen} onClose={closeModal}/>
             <div className={css.container}>
                 <h1>Calculate your daily calorie
                     intake right now</h1>
-                <form className={css.form} onSubmit={handleSubmit} >
+                <form className={css.form} onSubmit={handleSubmit}>
                     <div className={css.infoContainer}>
                         <div className={css.info}>
-                            <TextField className={css.textField} label="Height *" id="height"/>
-                            <TextField className={css.textField} label="Age *" id="age"/>
-                            <TextField className={css.textField} label="Current weight *" id="current-weight"/>
+                            <TextField className={css.textField} onChange={e => handleChange(+e.target.value, 'height')}
+                                type="number" label="Height *" id="height"/>
+                            <TextField className={css.textField} onChange={e => handleChange(+e.target.value, 'age')}
+                                type="number" label="Age *" id="age"/>
+                            <TextField className={css.textField}
+                                onChange={e => handleChange(+e.target.value, 'currentWeight')} type="number"
+                                label="Current weight *" id="current-weight"/>
                         </div>
                         <div className={css.info}>
-                            <TextField className={css.textField} label="Desired weight *" id="desired-weight"/>
+                            <TextField className={css.textField}
+                                onChange={e => handleChange(+e.target.value, 'desiredWeight')} type="number"
+                                label="Desired weight *" id="desired-weight"/>
                             <div className={css.bloodTypeContainer}>
                                 <span>Blood type*</span>
                                 <div>
-                                    <RadioGroup value={selectedValue} onChange={handleChange} name="blood-type"
+                                    <RadioGroup value={+formData['bloodType']}
+                                        onChange={e => handleChange(+e.target.value, 'bloodType')} name="blood-type"
                                         options={options}/>
                                 </div>
                             </div>
